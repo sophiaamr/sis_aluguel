7c4bce2226b2bee5bad925dc825e00fea4be54f5
sophiaamr <100172972+sophiaamr@users.noreply.github.com>
2024-09-13 17:07:36 -0300

    Adiciona projeto Spring Boot ao repositório

---
+++ sistemadealuguel/src/test/java/com/lab/sistemadealuguel/SistemadealuguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lab.sistemadealuguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemadealuguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

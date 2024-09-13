53f56a6b5cd9870d6c648f31f9088980dd4cffe8
sophiaamr <100172972+sophiaamr@users.noreply.github.com>
2024-09-13 17:45:18 -0300

    Inicializacao dando certo

---
+++ sis_aluguel/src/test/java/com/sisaluguel/SisAluguelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sisaluguel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisAluguelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
